6e15b9a9bf62282c1d7c1345d1e0eae12bc2d87b
Aitor Moreno <aitor@rojo2.com>
2015-12-16 18:51:24 +0100

    Fixed fluxGraph

--- src/app/ui/utils.js
+++ src/app/ui/utils.js
@@ -101,82 +101,80 @@ export function fluxGraph(el, data) {
   const rect = container.getBoundingClientRect();
 
   const margin = {
-        top: 20,
-        right: 20,
-        bottom: 30,
-        left: 50
-      },
-      width = rect.width - margin.left - margin.right,
-      height = rect.height - margin.top - margin.bottom,
-      sWidth = rect.width + margin.left,
-      sHeight = rect.height + margin.top;
+      top: 20,
+      right: 20,
+      bottom: 30,
+      left: 50
+    },
+    width = rect.width - margin.left - margin.right,
+    height = rect.height - margin.top - margin.bottom,
+    sWidth = rect.width + margin.left,
+    sHeight = rect.height + margin.top;
 
   const x = d3.time.scale()
-      .range([0, width]);
+    .range([0, width]);
 
   const y = d3.scale.log()
-      .base(10)
-      .range([height, 0]);
+    .base(10)
+    .range([height, 0]);
 
   const xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom")
-      .tickSize(-height, 0)
-      .tickPadding(6);
+    .scale(x)
+    .orient("bottom")
+    .tickSize(-height, 0)
+    .tickPadding(6);
 
   const yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left")
-      .tickSize(-width)
-      .tickPadding(6);
+    .scale(y)
+    .orient("left")
+    .tickSize(-width)
+    .tickPadding(6);
 
   const line = d3.svg.line()
-      .interpolate("basis")
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.value); });
+    .interpolate("basis")
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.value); });
 
   const area = d3.svg.area()
-      .interpolate("basis")
-      .x(function(d) { return x(d.date); })
-      .y0(function(d) { return y(1); })
-      .y1(function(d) { return y(d.value); });
+    .interpolate("basis")
+    .x(function(d) { return x(d.date); })
+    .y0(function(d) { return y(1); })
+    .y1(function(d) { return y(d.value); });
 
   const svg = d3.select(container)
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      /*.attr("width", "100%")
-      .attr("height", "100%")
-      .attr("viewBox","0 0 " + Math.max(sWidth,sHeight) + " " + Math.min(sWidth,sHeight))
-      .attr("preserveAspectRatio", "none")*/
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .append("svg")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("viewBox","0 0 " + Math.max(sWidth,sHeight) + " " + Math.min(sWidth,sHeight))
+    .attr("preserveAspectRatio", "none")
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   x.domain(d3.extent(data, function(d) { return d.date; }));
   y.domain([1,120000]);
 
   svg.append("path")
-      .datum(data)
-      .attr("class", "Graph__area")
-      .attr("d", area);
+    .datum(data)
+    .attr("class", "Graph__area")
+    .attr("d", area);
 
   svg.append("path")
-      .datum(data)
-      .attr("class", "Graph__line")
-      .attr("d", line);
+    .datum(data)
+    .attr("class", "Graph__line")
+    .attr("d", line);
 
   svg.append("g")
-      .attr("class", "Graph__axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    .attr("class", "Graph__axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
   svg.append("g")
-      .attr("class", "Graph__axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .style("text-anchor", "end")
-      .text("MeV");
+    .attr("class", "Graph__axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .style("text-anchor", "end")
+    .text("MeV");
 
 
 }
