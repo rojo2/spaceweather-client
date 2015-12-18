84211d2b1cb1058fe3f7df7dbb1a95faeb299268
Aitor Moreno <aitor@rojo2.com>
2015-12-18 11:51:50 +0100

    Fixed sunspots

--- src/app/ui/views/sunspots.js
+++ src/app/ui/views/sunspots.js
@@ -9,15 +9,17 @@ export function view(router) {
   utils.activate(utils.query("[href=\"/sunspots\"]"));
 
   const minDateFormatted = utils.daysFrom(-1),
-        maxDateFormatted = utils.daysFrom(-1);
+        maxDateFormatted = utils.daysFrom(-1),
+        minDateFormattedImage = utils.daysFrom(-2),
+        maxDateFormattedImage = utils.daysFrom(0);
 
   utils.activate(utils.query(".Loader", container));
   Promise.all([
 
     API.getImageChannels({
       channeltype: 5,
-      date_min: minDateFormatted,
-      date_max: maxDateFormatted
+      date_min: minDateFormattedImage,
+      date_max: maxDateFormattedImage
     }),
 
     API.getSunspotRegions({
@@ -27,8 +29,16 @@ export function view(router) {
 
   ]).then((res) => {
 
+    let images = [];
+    res[0].body.forEach((image) => {
+      if (new Date(image.date).getHours() === 17) {
+        images.push(image);
+        console.log(image);
+      }
+    });
+
     utils.deactivate(utils.query(".Loader", container));
-    utils.sunspots(container, res[0].body, res[1].body);
+    utils.sunspots(container, images, res[1].body);
 
   });
 
