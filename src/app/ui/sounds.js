c51382753fc7bd11a3080eff0c702b96f92a1ab9
Aitor Moreno <aitor@rojo2.com>
2016-01-14 19:30:34 +0100

    Added audio on/off button behaviour

--- src/app/ui/sounds.js
+++ src/app/ui/sounds.js
@@ -1,14 +1,23 @@
 export function initSounds() {
 
+  let volume = window.localStorage.getItem("volume") || 1.0,
+      muted = window.localStorage.getItem("muted") || false;
+
+  const ambient = sound("sounds/background.mp3", { loop: true });
+  ambient.muted = muted;
+  ambient.volume = volume;
+  ambient.play();
+
   function sound(src, options = {}) {
     options = Object.assign({
-      volume: 1.0,
+      volume: volume,
       loop: false,
-      mute: false
+      muted: muted
     }, options);
 
     const element = document.createElement("audio");
     element.src = src;
+    element.muted = options.muted;
     element.volume = options.volume;
     element.loop = options.loop;
     return element;
@@ -19,6 +28,35 @@ export function initSounds() {
     over.play();
   }
 
+  function updateSoundButton() {
+    const button = document.querySelector(".Button--sound");
+    if (muted) {
+      button.className = "Button--sound isOff";
+    } else {
+      button.className = "Button--sound isOn";
+    }
+  }
+
+  function updateAmbient() {
+    ambient.muted = muted;
+    ambient.volume = volume;
+  }
+
+  const button = document.querySelector(".Button--sound");
+  button.addEventListener("click", (e) => {
+
+    muted = !muted;
+
+    updateSoundButton(muted);
+    updateAmbient(muted);
+
+    window.localStorage.setItem("muted", muted);
+    window.localStorage.setItem("volume", volume);
+
+  });
+
+  updateSoundButton();
+
   document.addEventListener("visibilitychange", (e) => {
     if (document.visibilityState === "visible") {
       ambient.play();
@@ -27,16 +65,18 @@ export function initSounds() {
     }
   });
 
-  const ambient = sound("sounds/background.mp3", { loop: true });
-  ambient.play();
-
   Array.prototype.slice.call(document.querySelectorAll("[class*=item], [class*=menuItem]")).map((el) => {
     el.addEventListener("mouseenter", handleMouseEnter, false);
     return el;
   });
 
-  let volume = 1.0;
   return {
+    set muted(value) {
+      muted = value;
+    },
+    get muted() {
+      return muted;
+    },
     set volume(value) {
       ambient.volume = volume = value;
     },
